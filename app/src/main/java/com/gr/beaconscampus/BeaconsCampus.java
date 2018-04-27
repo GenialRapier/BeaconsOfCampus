5393864ccdd782e822ce38285a22e364b4275d96
ahmad.windardi80 <ahmad.windardi80@gmail.com>
2018-04-27 14:08:52 +0700

    First app commit

---
+++ app/src/main/java/com/gr/beaconscampus/BeaconsCampus.java
@@ -0,0 +1,49 @@
+package com.gr.beaconscampus;
+
+import android.app.Application;
+import android.content.Intent;
+import android.util.Log;
+
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.startup.BootstrapNotifier;
+import org.altbeacon.beacon.startup.RegionBootstrap;
+
+/**
+ * Created by ahmad on 19/04/2018.
+ */
+
+public class BeaconsCampus extends Application implements BootstrapNotifier {
+    private static final String TAG = ".MyApplicationName";
+    private RegionBootstrap regionBootstrap;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "App started up");
+        BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        Region region = new Region("com.example.myapp.boostrapRegion", null, null, null);
+        regionBootstrap = new RegionBootstrap(this, region);
+    }
+
+    @Override
+    public void didEnterRegion(Region region) {
+        Log.d(TAG, "Got a didEnterRegion call");
+        regionBootstrap.disable();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        this.startActivity(intent);
+    }
+
+    @Override
+    public void didExitRegion(Region region) {
+
+    }
+
+    @Override
+    public void didDetermineStateForRegion(int i, Region region) {
+
+    }
+}
---
+++ app/src/main/java/com/gr/beaconscampus/MainActivity.java
@@ -0,0 +1,23 @@
+package com.gr.beaconscampus;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
